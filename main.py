55b4f1abf780341725be4aaeec5f79cab820629d
Your Nameohdamnitsnikkki <you@example.commammastjernberg@gmail.com>
2025-10-16 20:27:43 +0000

    add dataFrame for excel and save prices to excel
    Co-authored-by: Ona <no-reply@ona.com>

--- main.py
+++ main.py
@@ -21,3 +21,16 @@ belopp_med_moms = belopp * 1.25
 # Visa resultat med max två decimaler
 print(f"\nBelopp utan moms: {belopp:.2f} kr")
 print(f"\nBelopp med 25% moms: {belopp_med_moms:.2f} kr")
+
+# Lägg till i listan för att kunna spara det i excel
+data.append({"Belopp utan moms": belopp, "Belopp med moms": round(belopp_med_moms, 2)})
+
+#Skapa datum till beloppen
+df = pd.DataFrame(data)
+
+
+# Spara till excel
+filnamn = "moms.xlsx"
+df.to_excel(filnamn, index=False)
+
+print(f"\n Priset har sparats i '{filnamn}'.")
