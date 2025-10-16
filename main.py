e64ac2ba9a6986aaaae21631dbe008fdd1d26dee
Your Nameohdamnitsnikkki <you@example.commammastjernberg@gmail.com>
2025-10-16 20:11:03 +0000

    show user amount and new amount with moms

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd # ger tillgång till excel´
 # Gör lista för resultat
 data = []
 
+# Loop som kontrollerar att användare endast fyller i siffror 
 while True:
     belopp_input = input("Skriv belopp i siffror: ")
 
@@ -10,4 +11,13 @@ while True:
         belopp = float(belopp_input)
         break
     except ValueError:
+        # Ger felmeddelande till användaren
         print("Ogiltigt belopp, använd endast siffror. Försök igen.\n")
+
+
+#Beräkna belopp med 25 % moms
+belopp_med_moms = belopp * 1.25
+
+# Visa resultat med max två decimaler
+print(f"\nBelopp utan moms: {belopp:.2f} kr")
+print(f"\nBelopp med 25% moms: {belopp_med_moms:.2f} kr")
